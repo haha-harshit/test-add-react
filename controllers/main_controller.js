425a8574581d41d09c1dbf7958f6ffbb691ab9c2
Harshit <bkharshitbk@gmail.com>
2021-06-15 19:48:36 +0530

    add controllers

---
+++ controllers/main_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req, res){
+    return res.send(
+    "<h1>I'm Happy!</h1>"
+    )
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const mainController = require('../controllers/main_controller');
+
+router.get('/', mainController.home);
+
 console.log('Router loaded!');
 
 module.exports = router;
